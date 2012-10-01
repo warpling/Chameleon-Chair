38c2aaf28aedf94e6cbddb1ab185f33c66d17d4b
Niels <hamelink@kth.se>
2012-10-01 13:54:00 +0200

    Added a new version of persondetector that should work from Arduino main library folder

--- PersonDetectorTest/PersonDetectorTest.ino
+++ PersonDetectorTest/PersonDetectorTest.ino
@@ -1,17 +1,21 @@
 #include <PersonDetector.h>
 
+
 PersonDetector detector(); // Initialize an instance of the class
 
 void setup()
 {
-  detector.calibrate(40);
+  //detector.calibrate(40);
+  detector.calibrate(/*10*/);
 }
 
 void loop()
 {
+  /*
   boolean isPerson = detector.isPersonPresent();
     
   Serial.println(isPerson);
+  */
   
   delay(250);
 }
