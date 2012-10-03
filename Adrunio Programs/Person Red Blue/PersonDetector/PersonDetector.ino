711f9ef8ecdf87ba172fed210d27245a1b04ce90
Ryan McLeod <ryanmcleod@gmail.com>
2012-10-03 17:07:47 +0200

    Updates to PersonDetector Library for testing the new placement of the thermal sensor.

--- Adrunio Programs/Person Red Blue/PersonDetector/PersonDetector.ino
+++ Adrunio Programs/Person Red Blue/PersonDetector/PersonDetector.ino
@@ -2,12 +2,13 @@
 #include <Printf.h>
 #include <PersonDetector.h>
 
-int SpotlightArray[] = {12, 11, 10};
+int SpotlightArray[] = {11, 10, 9};
 byte red[] = {0, 255, 255};
 byte green[] = {255, 0, 255};
 byte blue[] = {255, 255, 0};
 byte off[] = {255, 255, 255};
 
+boolean isPerson = false;
 boolean wasPerson = false;
 byte *newColor = blue;
 byte *lastColor = blue;
@@ -19,30 +20,49 @@ void setup()
 {
   Serial.begin(9600);
   Spotlight.set_Color(SpotlightArray, green);
-  detector.calibrate(20);
+  detector.calibrate(30);
+  Spotlight.set_Color(SpotlightArray, off);
 }
 
 void loop()
 {
-   boolean isPerson = detector.isPersonPresent();
+   isPerson = detector.isPersonPresent();
+   
+   
+   // keyboard testing code
+  /* 
+   if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    byte incomingByte = Serial.read();
+    // if it's a capital H (ASCII 72), turn on the LED:
+    if (incomingByte == 'a') {
+      isPerson = true;
+    } 
+    // if it's an L (ASCII 76) turn off the LED:
+    if (incomingByte == 'b') {
+      isPerson = false;
+    }
+  }
+  */
+   
    if(isPerson && !wasPerson) {
       newColor = red;
-      Spotlight.set_Color(SpotlightArray, blue);
+      Spotlight.set_Color(SpotlightArray, newColor);
       lastColor = newColor;
       wasPerson = true;
    }
    else if (!isPerson && wasPerson) {
       newColor = blue;
-      Spotlight.set_Color(SpotlightArray, red);
+      Spotlight.set_Color(SpotlightArray, newColor);
       lastColor = newColor;
       wasPerson = false;
    }
    else {
      // Do nothing
-     Spotlight.set_Color(SpotlightArray, blue);
+     Spotlight.set_Color(SpotlightArray, lastColor);
    }
    
-   delay(200);
+   delay(400);
 }
 
 
