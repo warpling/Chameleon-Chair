db1ddfb18e734ccb63b9ec478f04d92bf7e5ea23
Niels <hamelink@kth.se>
2012-10-05 10:13:40 +0200

    Updated theChair to check for more isPerson/wasPerson situations
    Also added som pseudocode for the colorspace calculations. This should
    be changed with the to be added saturation and brightness support.

--- Adrunio Programs/TheChair/TheChair.ino
+++ Adrunio Programs/TheChair/TheChair.ino
@@ -25,8 +25,8 @@ const byte tempSensorPin = A0;
 // 3. LED
 int LEDArray[] = {9,10,11}; // Red, Green, Blue
 LED LED1(LEDArray);
-//byte prevColor[3] = {0,0,0};
-byte endColor[3] = {0,0,0};
+byte newColor[3] = {0,0,0};  //In RGB
+byte referenceColor[3] = {0,0,0};  //In RGB
 
 // Target color components (holds R,G,B values the target color consists of)
 int __red;
@@ -34,11 +34,15 @@ int __green;
 int __blue;
 
 // Light intensity (how close are we to the target color)
-double intensity = 0.0;
+//double intensity = 0.0;
+int intensity;
+int maxBrightness = 100;  //Measured in percentages
+int brightnessChange = 10;
+int colorChangeDelay = 40;
 
 // Is there and was there a person in the chair?
-boolean isPerson;
-boolean wasPerson;
+boolean isPerson = false;
+boolean wasPerson = false;
 
 // Setup
 //   Main Calibration
@@ -63,7 +67,10 @@ void loop()
    //   Check if person is detected
    isPerson = detector.isPersonPresent();
    Serial_printf("Value: %d\n", isPerson);
-   if(isPerson)
+   
+   
+   
+   if(isPerson == true && wasPerson == false) //A new person sits down
    {
      // If person detected and wasn't there before, take color reading (do colorspace calculations, etc)     
      rgbObject = colorDetector.getColor();
@@ -73,28 +80,44 @@ void loop()
     
      Serial_printf("Target color value = %d - %d - %d\n", __red, __green, __blue);
      
-     //This color should be the endresult of the fade
-     endColor[0] = __red;
-     endColor[1] = __green;
-     endColor[2] = __blue;
+     //This color should be used as reference for the loop
+     referenceColor[0] = __red;
+     referenceColor[1] = __green;
+     referenceColor[2] = __blue;
      
-     // Next increment brightness
-     if(isPerson)
-       
+     //Convert referenceColor to HSB with brightness and saturation set to very low intensity, this will be the newColor that is set as initialization
+     /*
+     newColor[0] = ;
+     newColor[1] = ;
+     newColor[2] = ;
+     */
      
-     //LED1.set_Color(LEDArray, );
+     //After calculations set the color of the chair to the new color
+     //LED1.set_Color(LEDArray, newColor);
+     delay(colorChangeDelay);
      
-     
-     //Set some historic context
+     //Set some historic context, there was a person here
      wasPerson = true;
-     
-   }else if (isPerson == false /*&& wasPerson == true*/)
+   }
+   else if(isPerson == true && wasPerson == true)  //Person still sitting there
    {
-     // If person no longer sitting, decrement brightness
-     delay(500);
+     // Increment brightness
+     //LED1.set_Color(LEDArray, newColor);
+     //change intermediate color variable by convert to HSB and add standard brightnessvalue
+     //newColor[3] = newColor[3] + brightnessChange;
+     delay(colorChangeDelay);
+     
+   }else if (isPerson == false && wasPerson == true)
+   {
+     // If person no longer sitting, decrement brightness after delay
+     // Delay could be added here
      
      
-     //wasPerson = false;
+     //Chnange intermediate color variable
+     delay(colorChangeDelay);
+     
+     
+     wasPerson = false;
    }
    delay(250);
 }
