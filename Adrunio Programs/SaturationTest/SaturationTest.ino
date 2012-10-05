bd460fdeb22b857182da296e45e8da1e16805b4f
Ryan McLeod <ryanmcleod@gmail.com>
2012-10-05 13:56:05 +0200

    Updated Saturator Test Code

--- Adrunio Programs/SaturationTest/SaturationTest.ino
+++ Adrunio Programs/SaturationTest/SaturationTest.ino
@@ -6,9 +6,9 @@ Saturator saturator = Saturator();
 void setup() {
   Serial.begin(9600);
   
-  rgb_color testColor = {50, 100, 200};
+  rgb_color testColor = {150, 20, 220};
   rgb_color newColor = saturator.saturate(testColor.r, testColor.g, testColor.b);
-  Serial_printf("[%d, %d, %d]\n", newColor.r, newColor.g, newColor.b);
+  Serial_printf("HSV [%d, %d, %d]\n", newColor.r, newColor.g, newColor.b);
 }
 
 void loop() {
