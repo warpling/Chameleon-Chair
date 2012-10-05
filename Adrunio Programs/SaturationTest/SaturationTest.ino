b48474b698bf52d859c00e00f5170688d8188603
Ryan McLeod <ryanmcleod@gmail.com>
2012-10-05 13:08:31 +0200

    Changed test value

--- Adrunio Programs/SaturationTest/SaturationTest.ino
+++ Adrunio Programs/SaturationTest/SaturationTest.ino
@@ -6,7 +6,7 @@ Saturator saturator = Saturator();
 void setup() {
   Serial.begin(9600);
   
-  rgb_color testColor = {256, 128, 64};
+  rgb_color testColor = {50, 100, 200};
   rgb_color newColor = saturator.saturate(testColor.r, testColor.g, testColor.b);
   Serial_printf("[%d, %d, %d]\n", newColor.r, newColor.g, newColor.b);
 }
